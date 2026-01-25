3ffaef9781484abfa31e4474225f708344d1c7ca
Kaipo Chen <kapoo1933@gmail.com>
2026-01-25 11:38:53 +0800

    feat: Add lazygit.nvim integration for git management
    Adds lazygit.nvim with <leader>gg keybinding for quick access to the
    lazygit TUI within Neovim.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "kdheepak/lazygit.nvim",
+    cmd = { "LazyGit", "LazyGitConfig", "LazyGitCurrentFile", "LazyGitFilter", "LazyGitFilterCurrentFile" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+  {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewOpen", "DiffviewFileHistory" },
     keys = {
